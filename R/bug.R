e130e5dee4ef04b75dbf2f81323c0a92320d9d85
martinlaw <martin.law@mrc-bsu.cam.ac.uk>
2021-03-30 12:32:44 +0100

    rm'd + signs

--- R/bug.R
+++ R/bug.R
@@ -1,5 +1,5 @@
 # Bug when bounds=NULL and fixed.r given:
  block8.opt <- twoarmDesign(nmin.arm=112/2, nmax.arm=112/2, block.size=8, pc=0.3,
-+                           pt=0.5, alpha=0.15, power=0.8,fixed.r=5,bounds=NULL,
-+                           exact.thetaF=0.3005, exact.thetaE=0.97)
+                           pt=0.5, alpha=0.15, power=0.8,fixed.r=5,bounds=NULL,
+                           exact.thetaF=0.3005, exact.thetaE=0.97)
 
