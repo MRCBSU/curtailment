409c2e98869e780ab459b4b094702ec4d09ca50d
martinlaw <martin.law@mrc-bsu.cam.ac.uk>
2021-09-23 16:10:26 +0100

    Split singlearm_functions.R into separate files

---
+++ R/imports.R
@@ -0,0 +1,8 @@
+#' @import data.table
+#' @import ggplot2
+#' @import ggthemes
+#' @import pkgcond
+#' @import stats
+#' @importFrom grDevices dev.off pdf
+#' @importFrom utils combn head setTxtProgressBar txtProgressBar
+#requireNamespace("data.table")
